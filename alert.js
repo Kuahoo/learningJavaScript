9df2bde396f8584f4b71c962f8a1eb5dae32b3ea
Kuahoo <kuahoo@pop-os.localdomain>
2024-03-01 17:19:04 -0800

    Javscript.info: The name of JavaScript

--- alert.js
+++ alert.js
@@ -1,3 +1,7 @@
 'use strict';
-let promptText = prompt('What is you name?', '');
-alert(promptText);
+let promptText = prompt('What\'s the "official" name of Javascript?', '');
+if(promptText === 'ECMAScript'){
+    alert('Right!');
+}else{
+    alert('You don\'t know "ECMAScript"!');
+}
